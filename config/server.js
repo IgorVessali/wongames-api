a2db560b5612145f8167dc3f8cf053ced67a9661
Igor Vessali <igorvessali@gmail.com>
2020-12-30 12:03:10 -0300

    Instalado o STRAPI

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd565690c278019f60414ed783e2e70e4'),
+    },
+  },
+});
